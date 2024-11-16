4cee977d2c9c67e1c888adc0c87d03512cf8d846
Daniel <mills.daniel320@gmail.com>
2024-11-16 16:46:16 -0600

    inital commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const quizContent = [
+    {
+        question: 'What is your favorite color',
+        a: 'Red',
+        b: 'Green',
+        c: 'Yellow',
+        d: 'Black',
+        correctAnswer: 'B'
+    }
+]
+
+//Grab a question
+const question = quizContent[0/*Math.floor(Math.random() * 10)*/]
+console.log(question)
+//Each Question should have a group of answers
+//Each Question Should have a correct answer
+//User should be notified to guess again if they get the wrong answer
+//Notified if they get the correct answer
+//Button to go to the next question if answer is correct
+//Lets user know when quiz is over
+//Button to restart the quiz
+//Function that creates the quiz when a button is pressed and starts it
+function startQuiz() {
+
+}
+
+function createQuiz() {
+
+}
+
+function restartQuiz() {
+
+}
+
+function checkAnswer() {
+    
+}
