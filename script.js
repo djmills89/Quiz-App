7de291c1151acf1947c5918c00e60f1a6634afde
Daniel <mills.daniel320@gmail.com>
2024-11-18 20:53:08 -0600

    Added questions about cosmere, shuffle array function added

--- script.js
+++ script.js
@@ -1,11 +1,78 @@
 const quizContent = [
     {
-        question: 'What is your favorite color',
-        a: 'A. Red',
-        b: 'B. Green',
-        c: 'C. Yellow',
-        d: 'D. Black',
-        correctAnswer: 'B. Green'
+        question: 'What metal, when burned by an allomancer, enhances their senses?',
+        a: 'A. Cadmium',
+        b: 'B. Tin',
+        c: 'C. Pewter',
+        d: 'D. Gold',
+        correctAnswer: 'B. Tin'
+    },
+    {
+        question: 'What metalmind would a Keeper use to store physical weight?',
+        a: 'A. Copper',
+        b: 'B. Brass',
+        c: 'C. Iron',
+        d: 'D. Electrum',
+        correctAnswer: 'C. Iron'
+    },
+    {
+        question: 'What metal can be burned by anyone and make the individual a Mistborn?',
+        a: 'A. Steel',
+        b: 'B. Zinc',
+        c: 'C. Bronze',
+        d: 'D. Lerasium',
+        correctAnswer: 'D. Lerasium'
+    },
+    {
+        question: 'What shape do Logicspren take in the Physical Realm on Roshar?',
+        a: 'A. Objects they recently saw',
+        b: 'B. Tiny Stormclouds',
+        c: 'C. Tiny, Red Insect-like',
+        d: 'D. Brown Specks with Wings',
+        correctAnswer: 'B. Tiny Stormclouds'
+    },
+    {
+        question: 'If you had access to the Surges of Illumination and Progression, what Knight Radiant order would you belong to?',
+        a: 'A. Truthwatchers',
+        b: 'B. Stonewards',
+        c: 'C. Windrunners',
+        d: 'D. Lightweavers',
+        correctAnswer: 'A. Truthwatchers'
+    },{
+        question: 'What is the third ideal Dalinar speaks?',
+        a: 'A. I will unite instead of divide. I will bring men together.',
+        b: 'B. I will protect even those I hate so long as it is right',
+        c: 'C. I will take responsibility for what I have done. If I must fall, I will rise each time a better man.',
+        d: 'D. I am a stick.',
+        correctAnswer: 'C. I will take responsibility for what I have done. If I must fall, I will rise each time a better man.'
+    },{
+        question: 'Other than Awakening, what other ability does Vivenna have in Warbreaker?',
+        a: 'A. Surgebinding',
+        b: 'B. Forgery',
+        c: 'C. The Force',
+        d: 'D. Royal Locks',
+        correctAnswer: 'D. Royal Locks'
+    },{
+        question: 'What did Lightsong the Bold do in his previous life?',
+        a: 'A. Detective',
+        b: 'B. Chef',
+        c: 'C. Accountant',
+        d: 'D. Clown',
+        correctAnswer: 'C. Accountant'
+    },{
+        question: 'What Shard did the vessel Edgli hold?',
+        a: 'A. Odium',
+        b: 'B. Endowment',
+        c: 'C. Ambition',
+        d: 'D. Ruin',
+        correctAnswer: 'B. Endowment'
+    },{
+        question: 'What does the Aon Rao mean?',
+        a: 'A. Spirit',
+        b: 'B. Love',
+        c: 'C. The Direction East',
+        d: 'D. Focus',
+        correctAnswer: 'A. Spirit'
     }
 ]
 
@@ -14,7 +81,17 @@ const startBtn = document.getElementById('startBtn')
 
 //Grab a question
 const question = quizContent[0/*Math.floor(Math.random() * 10)*/]
-console.log(question)
+function shuffleQuiz(array) {
+    for(let i = array.length - 1; i >= 0; i--) {
+        let j = Math.floor(Math.random() * i + 1)
+        let temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
+    }
+    return array
+}
+const quizShuffled = shuffleQuiz(quizContent)
+console.log(quizShuffled)
 //Each Question should have a group of answers
 //Each Question Should have a correct answer
 //User should be notified to guess again if they get the wrong answer
